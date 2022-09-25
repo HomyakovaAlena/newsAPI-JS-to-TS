afb4d700429057da5c8e2c9a7e6cf213eb12b4bb
Alena Homyakova <homiakovaelena@gmail.com>
2022-09-25 12:31:37 +0300

    init JS project with API key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'c75b740205d64e729211a583a172d058',
         });
     }
 }
